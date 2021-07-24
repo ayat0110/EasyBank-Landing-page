1e1af91c0a00f3d0d449f3aa6f655c42fc64afbe
Ayat AL-ZAIDI <76471156+ayat0110@users.noreply.github.com>
2021-07-24 15:37:39 +0300

    Finished Hamburger Menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,18 +1,30 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
-
+const fadeElms = document.querySelectorAll('.has_fade')
 btnHamburger.addEventListener('click',function(){
     console.log('open hamburger');
 
     if(header.classList.contains('open')){ // close hamburger menu
-    header.classList.remove('open');
-    overlay.classList.remove('fade-in');
-    overlay.classList.add('fade-out');
+        body.classList.remove('noscroll');
+        header.classList.remove('open');
+    fadeElms.forEach(function(element){
+        element.classList.remove('fade-in');
+        element.classList.add('fade-out');
+    });
+   
     }
     else{ // open hamburger menu
+        body.classList.add('noscroll');
         header.classList.add('open');
-        overlay.classList.remove('fade-out');
-        overlay.classList.add('fade-in');
+        fadeElms.forEach(function(element){
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        }
+        
+        );
+
+        
     }
 });
